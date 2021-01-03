7e16ab1cbd610adf7f0449c6faf745c6e9fcfe5f
fangcherwei <example@mail.com>
2021-01-03 14:05:16 +0800

    Add firebase.js to initialise app

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyBOTdACA2i2d3sAWnPhNfcJi3ZXb8_qoxM",
+    authDomain: "itrack-bf402.firebaseapp.com",
+    projectId: "itrack-bf402",
+    storageBucket: "itrack-bf402.appspot.com",
+    messagingSenderId: "672254168808",
+    appId: "1:672254168808:web:206379a0337d4aa3f3c35d",
+    measurementId: "G-0GB2F720BW"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
